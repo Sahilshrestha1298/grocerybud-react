e43721174da7f82a93af0c2cc2c00456bbe106e6
Sahilshrestha1298 <shresthasahil24@gmail.com>
2026-02-16 21:31:36 +0545

    adding app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Items from "./components/Items";
 import { groceryItems } from "./data/groceryitems";
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [items, setItems] = useState(groceryItems);
+
+  const editCompleted = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
   return (
     <section className="section-center">
-      <Items items={groceryitems} />
+      <Items items={groceryItems} />
     </section>
   );
 };
