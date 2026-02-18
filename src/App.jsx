becbb87b129430bf93ef70b9c2408efa6d530ccf
Sahilshrestha1298 <shresthasahil24@gmail.com>
2026-02-18 11:28:22 +0545

    updating form and single item jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Items from "./components/Items";
 import { groceryItems } from "./data/groceryitems";
-import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
