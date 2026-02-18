485b93deb82c07031c1b29de660536e0768102bb
Sahilshrestha1298 <shresthasahil24@gmail.com>
2026-02-18 11:30:43 +0545

    update app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import Items from "./components/Items";
-import { groceryItems } from "./data/groceryitems";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { nanoid } from "nanoid";
-import Form from "./components/form";
-import { useEffect, useRef, useState } from "react";
+import Form from "./components/Form";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("grocery-list");
+  if (list) {
+    return JSON.parse(list);
+  }
+  return [];
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem("grocery-list", JSON.stringify(items));
+};
+
+const initialList = getLocalStorage();
 
 const App = () => {
-  const [items, setItems] = useState(groceryItems);
+  const [items, setItems] = useState(initialList);
   const [editId, setEditId] = useState(null);
   const inputRef = useRef(null);
 
@@ -26,7 +39,8 @@ const App = () => {
     };
     const newItems = [...items, newItem];
     setItems(newItems);
-    toast.success("grocery item added");
+    setLocalStorage(newItems);
+    toast.success("item added to the list");
   };
 
   const editCompleted = (itemId) => {
@@ -37,22 +51,25 @@ const App = () => {
       return item;
     });
     setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
     toast.success("item deleted");
   };
 
-  const updateItemName = (newName) => {
+  const updateItemName = (itemEditName) => {
     const newItems = items.map((item) => {
       if (item.id === editId) {
-        return { ...item, name: newName };
+        return { ...item, name: itemEditName };
       }
       return item;
     });
     setItems(newItems);
+    setLocalStorage(newItems);
     setEditId(null);
     toast.success("item updated");
   };
@@ -76,4 +93,5 @@ const App = () => {
     </section>
   );
 };
+
 export default App;
